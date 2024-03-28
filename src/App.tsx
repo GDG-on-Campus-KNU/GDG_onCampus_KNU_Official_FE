842f8f21a7bb8e5fd2af66ffd037411925f2486d
KimKyuHoi <k546kh@gmail.com>
2024-03-28 15:27:34 +0900

    Feat: 기본 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './App.css';
-import viteLogo from '/vite.svg';
+import ErrorPage from './pages/ErrorPage';
+import MainPage from './pages/MainPage';
+import MyPage from './pages/MyPage';
+import RootPage from './pages/RootPage';
+import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
 
-import reactLogo from './assets/react.svg';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootPage />,
+    id: 'root',
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <MainPage /> },
+      { path: 'signin', element: <SigninPage /> },
+      { path: 'signup', element: <SignupPage /> },
+      { path: 'mypage/:username', element: <MyPage /> },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   );
 }
