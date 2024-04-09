eecd6d9d4977b05e8fe1823ca6b20686585bc6fd
KimKyuHoi <k546kh@gmail.com>
2024-04-09 15:56:09 +0900

    Refactor: App.tsx, main.tsx 코드 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+import { queryClient } from './hooks/queries/Http';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
