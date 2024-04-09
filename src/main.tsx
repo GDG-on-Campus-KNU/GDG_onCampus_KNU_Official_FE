1705ebeb913242e8785298aba0087b8b7ace5401
KimKyuHoi <k546kh@gmail.com>
2024-04-09 15:32:26 +0900

    Fix: EsLint 문법 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
