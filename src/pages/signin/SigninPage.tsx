c3b99144ff697fe3c49d9b8ad2c748d376e41aee
KimKyuHoi <k546kh@gmail.com>
2024-04-25 16:17:52 +0900

    Chore: 로그인 파일 이동

---
+++ src/pages/signin/SigninPage.tsx
@@ -0,0 +1,11 @@
+import SigninModal from '@gdsc/pages/signin/components/SigninModal';
+
+const SigninPage = () => {
+  return (
+    <>
+      <SigninModal />
+    </>
+  );
+};
+
+export default SigninPage;
