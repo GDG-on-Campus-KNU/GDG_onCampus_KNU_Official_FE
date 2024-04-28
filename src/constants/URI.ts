a7dd9570c1301d3f7c630ade6005c6385d168901
KimKyuHoi <k546kh@gmail.com>
2024-04-28 22:43:03 +0900

    Feat: 구글 LoginURL 추가

--- src/constants/URI.ts
+++ src/constants/URI.ts
@@ -1,1 +1,3 @@
 export const BASE_URI = import.meta.env.VITE_APP_BASE_URL;
+export const GOOGLE_AUTH_CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_AUTH_CLIENT_ID;
+export const GOOGLE_AUTH_REDIRECT_URI = import.meta.env.VITE_APP_GOOGLE_AUTH_REDIRECT_URI;
--- src/pages/signin/components/SigninModal.tsx
+++ src/pages/signin/components/SigninModal.tsx
@@ -1,14 +1,25 @@
 import SigninBtn from '@gdsc/components/Button/SigninBtn';
 import AuthModal from '@gdsc/components/auth/AuthModal';
+import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_REDIRECT_URI } from '@gdsc/constants/URI';
 
 const SigninModal = () => {
+
+  const GoogleLoginUrl = `
+    https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?
+    response_type=code&client_id=${GOOGLE_AUTH_CLIENT_ID}&
+    redirect_uri=${GOOGLE_AUTH_REDIRECT_URI}&scope=openid email profile
+  `;
+  const google_login = () => {
+    window.open(GoogleLoginUrl, '_self');
+  };
+
   return (
     <>
       <AuthModal
         title='GDSC 로그인'
         text={`Google 계정을 이용하여\n GDSC KNU에 로그인하세요.`}
       >
-        <SigninBtn width='80%'>Google 계정으로 로그인</SigninBtn>
+        <SigninBtn onClick={google_login} width='80%'>Google 계정으로 로그인</SigninBtn>
       </AuthModal>
     </>
   );
