59025828ec41382369f2d8f091ceb2a75cce495d
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2024-05-06 18:39:57 +0900

    푸터 최종 구현 (#22)
    * Chore: tsconfig.node.json 경로 버그 수정 및 babel설치
    
    * Feat: PC버전 푸터 구현
    
    * Feat: 모바일 버전 푸터 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,12 @@
+import { useMediaQuery } from 'react-responsive';
+
+import MainFooter from '@gdsc/components/common/MainFooter';
+import MobileFooterMobile from '@gdsc/components/common/MainFooterMobile';
+
 const MainPage = () => {
-  return <>123</>;
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+
+  return <>{isMobile ? <MobileFooterMobile /> : <MainFooter />}</>;
 };
 
 export default MainPage;
