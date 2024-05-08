e64d208833e6dc0b95c894818232577fd89e92e1
KimKyuHoi <k546kh@gmail.com>
2024-05-09 04:59:01 +0900

    Chore: 폴더 경로 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import { useMediaQuery } from 'react-responsive';
 
-import MainFooter from '@gdsc/components/common/MainFooter';
-import MobileFooterMobile from '@gdsc/components/common/MainFooterMobile';
+import MainFooter from '@gdsc/components/common/footer/MainFooter';
+import MobileFooterMobile from '@gdsc/components/common/footer/MainFooterMobile';
 
 const MainPage = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
