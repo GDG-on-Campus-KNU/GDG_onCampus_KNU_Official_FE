f8db754d6724265c4a6a8e68b9ceca2d612e971f
KimKyuHoi <k546kh@gmail.com>
2024-05-13 18:46:14 +0900

    feat: 메인페이지 추가

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -3,10 +3,33 @@ import { useMediaQuery } from 'react-responsive';
 import MainFooter from '@gdsc/components/common/footer/MainFooter';
 import MobileFooterMobile from '@gdsc/components/common/footer/MainFooterMobile';
 
+import MainRound from '@gdsc/pages/main/components/MainRound';
+import Star from '@gdsc/pages/main/components/Star';
+
+import styled from '@emotion/styled';
+
+const DisplayLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
 const MainPage = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
 
-  return <>{isMobile ? <MobileFooterMobile /> : <MainFooter />}</>;
+  return (
+    <DisplayLayout>
+      <MainRound />
+      {[...Array(15)].map((_, index) => (
+        <Star
+          key={index}
+          top={`${Math.random() * 30}%`}
+          left={`${Math.random() * 100}%`}
+        />
+      ))}
+      {isMobile ? <MobileFooterMobile /> : <MainFooter />}
+    </DisplayLayout>
+  );
 };
 
 export default MainPage;
