b32dfcafdfcb35af2c80b60196b343723e28c673
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2024-05-19 14:47:33 +0900

    마이페이지 구현 (#31)
    * Refac: input창 + 라벨 합치기 작업
    
    * Style: 마이페이지 카드 구현
    
    * Style: 마이페이지 이름, 자기소개 입력창 구현
    
    * Feat: 프로필 이미지 업로드 기능 구현 및 프로필 컴포넌트 분리
    
    * style: 소속 팀 토큰 구현
    
    * style: 나이, 전공, 학번, 이메일 입력창 및 저장 버튼 추가
    
    * Style: 마이페이지 모바일 버전 구현
    
    * Feat: textarea 현재 글자수 및 경고메시지 기능 구현, 500자 이상 붙여넣기 안됨 오류 해결
    
    * Fix: textarea 경고메시지 사이렌 크기 수정
    
    ---------
    
    Co-authored-by: 김규회 <48755156+KimKyuHoi@users.noreply.github.com>

---
+++ src/components/form/Profile.tsx
@@ -0,0 +1,93 @@
+import { useState, useRef, ChangeEvent } from 'react';
+
+import DefaultProfile from '@gdsc/assets/DefaultProfile.svg';
+import PencilIcon from '@gdsc/assets/EditIcon.svg';
+
+import { displayCenter } from '@gdsc/styles/LayoutStyle';
+
+import styled from '@emotion/styled';
+
+const ProfileContainer = styled.div`
+  ${displayCenter}
+  align-items: center;
+  position: relative;
+
+  width: 80px;
+  height: 80px;
+
+  border-radius: 50%;
+
+  background-color: var(--color-que);
+`;
+
+const ProfileImg = styled.img`
+  width: 80px;
+  height: 80px;
+
+  border-radius: 50%;
+`;
+
+const EditBtn = styled.label`
+  ${displayCenter}
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  width: 32px;
+  height: 32px;
+
+  background-color: var(--color-white);
+
+  border: none;
+  border-radius: 50%;
+  box-shadow: 2px 3px 9px rgba(0, 0, 0, 0.15);
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const EditIcon = styled.img`
+  color: var(--color-revolver);
+`;
+
+const InputFile = styled.input`
+  display: none;
+`;
+
+const Profile = () => {
+  const [profileImage, setProfileImage] = useState(DefaultProfile);
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const result = reader.result as string;
+        setProfileImage(result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <ProfileContainer>
+      <ProfileImg src={profileImage} alt='profile' />
+      <EditBtn htmlFor='upload-input'>
+        <EditIcon src={PencilIcon} alt='edit' />
+      </EditBtn>
+      <InputFile
+        type='file'
+        id='upload-input'
+        ref={inputFileRef}
+        onChange={handleImageUpload}
+      />
+    </ProfileContainer>
+  );
+};
+
+export default Profile;
---
+++ src/components/form/TextArea.tsx
@@ -0,0 +1,186 @@
+import { useState } from 'react';
+
+import Text from '../typography/Text';
+import styled from '@emotion/styled';
+
+interface ITextArea {
+  id: string;
+  label: string;
+  placeholder: string;
+  maxLength: number;
+  color?: string;
+}
+
+const TextAreaWrapper = styled.div`
+  width: 80%;
+  height: 284px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+const TextAreaLabel = styled.label`
+  font-size: var(--font-size-md);
+  font-weight: bold;
+
+  font-family: 'Noto+Sans';
+`;
+
+const TextAreaContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const TextAreaElement = styled.textarea<{ color?: string; hasError?: boolean }>`
+  width: 100%;
+  height: 100%;
+
+  padding: 12px;
+  margin-top: 6px;
+
+  border: ${(props) => (props.hasError ? '1px solid red' : 'none')};
+  border-radius: 12px;
+  box-sizing: border-box;
+
+  font-size: var(--font-size-md);
+  font-family: 'Noto+Sans';
+
+  background: ${(props) => props.color || 'var(--color-que)'};
+  opacity: 1;
+  color: var(--color-white);
+
+  &::placeholder {
+    color: var(--color-dove);
+    font-size: var(--font-size-xs);
+  }
+
+  &:focus {
+    color: white;
+    outline: none;
+  }
+
+  &::-webkit-scrollbar {
+    width: 7px;
+    cursor: pointer;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: var(--color-abony);
+    border-radius: 12px;
+    border: none;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--color-dove);
+    border-radius: 12px;
+    border: none;
+  }
+`;
+
+const GradientOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+  width: 100%;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 40px;
+    background: linear-gradient(to bottom, rgba(31, 22, 50, 0.8), transparent);
+  }
+`;
+
+const CharacterCount = styled.div<{ hasError?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  align-self: flex-end;
+
+  width: 100%;
+  margin-top: 20px;
+
+  font-size: var(--font-size-sm);
+  color: ${(props) => (props.hasError ? 'red' : 'var(--color-dove)')};
+  font-family: 'Noto+Sans';
+`;
+
+const TextArea: React.FC<ITextArea> = ({
+  id,
+  label,
+  maxLength,
+  placeholder,
+  color,
+}) => {
+  const [text, setText] = useState('');
+
+  const handleTextLength = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = event.target;
+    if (value.length <= maxLength + 1) {
+      setText(value);
+    }
+  };
+
+  const handlePaste = (event: React.ClipboardEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+    const paste = event.clipboardData.getData('text');
+    const textToPaste = paste.slice(0, maxLength - text.length);
+    setText((prevText) => prevText + textToPaste);
+  };
+
+  return (
+    <TextAreaWrapper>
+      <TextAreaLabel htmlFor={id}>{label}</TextAreaLabel>
+      <TextAreaContainer>
+        <TextAreaElement
+          id={id}
+          placeholder={placeholder}
+          color={color}
+          value={text}
+          onChange={handleTextLength}
+          onPaste={handlePaste}
+          hasError={text.length > maxLength}
+        />
+        <GradientOverlay />
+      </TextAreaContainer>
+      <CharacterCount hasError={text.length > maxLength}>
+        {text.length > maxLength ? (
+          <div>
+            <Text size='lg'>🚨</Text>
+            <Text size='sm' color='red' weight='bold'>
+              {' '}
+              500자를 초과하였습니다.
+            </Text>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        <div>
+          <Text
+            size='sm'
+            weight='bold'
+            color={
+              text.length > maxLength
+                ? 'var(--color-red)'
+                : 'var(--color-white)'
+            }
+          >
+            {text.length}
+          </Text>
+          /{maxLength}
+        </div>
+      </CharacterCount>
+    </TextAreaWrapper>
+  );
+};
+
+export default TextArea;
