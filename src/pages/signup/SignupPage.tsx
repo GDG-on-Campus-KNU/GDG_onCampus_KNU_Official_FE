8e3f4d4279d43ff584ef8f3c873d6e67426efbed
KimKyuHoi <k546kh@gmail.com>
2024-06-28 22:37:01 +0900

    Design: SignupPage구현

--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -1,10 +1,12 @@
 import SignUpForm from '@gdsc/pages/signup/components/SignUpForm';
 
+import { AuthWrapper } from '@gdsc/styles/AuthModalStyle';
+
 const SignupPage = () => {
   return (
-    <>
+    <AuthWrapper>
       <SignUpForm />
-    </>
+    </AuthWrapper>
   );
 };
 
