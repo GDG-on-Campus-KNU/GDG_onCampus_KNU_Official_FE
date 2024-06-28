e0141ce410c1ae8e1a002747f79bc8170cd2999f
KimKyuHoi <k546kh@gmail.com>
2024-06-29 03:52:33 +0900

    Feat: 인터페이스 구현

--- src/interface/ButtonInterfaces.ts
+++ src/interface/ButtonInterfaces.ts
@@ -2,7 +2,20 @@ export interface IButton {
   color: string;
   backgroundColor: string;
   hoverColor: string;
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | undefined;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | undefined;
   type: 'button' | 'submit' | 'reset' | undefined;
   children: React.ReactNode;
+}
+
+export interface ICButton extends IButton {
+  width: string;
+  height: string;
+  mdWidth?: string;
+  mdHeight?: string;
+  mdSize?: string;
+  mWidth?: string;
+  mHeight?: string;
+  padding: string;
+  mPadding?: string;
+  mSize?: string;
 }
