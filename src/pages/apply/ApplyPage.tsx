4a0e455a8172781de92e068113e5353cb76e1cb4
KimKyuHoi <k546kh@gmail.com>
2024-06-28 22:54:39 +0900

    Design: Layout CSS 구현

--- src/pages/apply/ApplyPage.tsx
+++ src/pages/apply/ApplyPage.tsx
@@ -1,5 +1,15 @@
+import { DisplayLayout } from '@gdsc/styles/LayoutStyle';
+
+import styled from '@emotion/styled';
+
+const ApplyLayout = styled(DisplayLayout)`
+  height: calc(100vh - 45px);
+  display: flex;
+  justify-content: center;
+`;
+
 const ApplyPage = () => {
-  return <>지원하기 페이지</>;
+  return <ApplyLayout>123</ApplyLayout>;
 };
 
 export default ApplyPage;
