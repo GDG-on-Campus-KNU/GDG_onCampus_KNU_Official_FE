113271542f1f01691dd970242cc2431f9cbb9560
KimKyuHoi <k546kh@gmail.com>
2024-06-29 03:53:14 +0900

    Design: 지원 공용 스타일 나누기

---
+++ src/styles/ApplyStyle.ts
@@ -0,0 +1,49 @@
+import Text from '@gdsc/components/typography/Text';
+import Title from '@gdsc/components/typography/Title';
+
+import styled from '@emotion/styled';
+
+export const ApplyLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    margin: 0px 62px;
+  }
+
+  @media (max-width: 500px) {
+    margin: 0px 32px;
+  }
+`;
+
+export const TitleLayout = styled.div`
+  margin-bottom: 44.12px;
+
+  @media (max-width: 500px) {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+    margin-bottom: 80px;
+  }
+`;
+
+export const MainTitle = styled(Title)`
+  font-size: var(--font-size-xxl);
+  font-weight: 700;
+`;
+
+export const SubTitle = styled(Text)`
+  font-size: var(--font-size-xl);
+  font-weight: 700;
+  margin-right: 5px;
+`;
+
+export const Explain = styled.span`
+  font-size: var(--font-size-sm);
+  font-weight: 300;
+`;
+
+export const SubLayout = styled.div`
+  margin-bottom: 10px;
+`;
