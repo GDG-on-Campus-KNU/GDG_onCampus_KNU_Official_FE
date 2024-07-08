3f43b6c2b51e56c606bbe93de293fc3a2dc809c1
KimKyuHoi <k546kh@gmail.com>
2024-07-08 13:32:11 +0900

    Feat: Apply 인터페이스 생성

--- src/interface/ApplyInterface.ts
+++ src/interface/ApplyInterface.ts
@@ -8,3 +8,37 @@ export interface ApplyExInterface {
   prefer: string[];
   event: string[];
 }
+
+export interface ApplyFormInterface {
+  name: string;
+  studentNumber: string;
+  major: string;
+  email: string;
+  phoneNumber: string;
+  techStack: string;
+  links: string;
+  answers: {
+    questionNumber: number;
+    answer: string;
+  }[];
+}
+
+export interface ApplyFormQuestionInterface {
+  korean: string;
+  english: string;
+  Question1: {
+    main: string;
+  };
+  Question2: {
+    main: string;
+    sub: string;
+  };
+  Question3: {
+    main: string;
+    sub: string;
+  };
+  Question4: {
+    main: string;
+    sub: string;
+  };
+}
