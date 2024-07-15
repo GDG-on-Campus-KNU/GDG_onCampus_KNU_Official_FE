c120a4bd5dfbf27be66fa879ded0b9f9d1e2acfe
KimKyuHoi <k546kh@gmail.com>
2024-07-15 16:13:06 +0900

    Design: CSS 수정

--- src/pages/signup/components/SignUpForm.tsx
+++ src/pages/signup/components/SignUpForm.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { useForm } from 'react-hook-form';
 
 import CommonBtn from '@gdsc/components/button/CommonBtn';
@@ -15,36 +16,10 @@ import { ErrorMessage } from '@hookform/error-message';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const SignFormWrapper = styled.form`
-  width: 90%;
-
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  grid-template-columns: 1fr 1fr 2fr;
-  column-gap: 30px;
-  row-gap: 25px;
-
-  & > *:nth-child(3) {
-    width: 100%;
-  }
-  & > *:nth-child(4) {
-    grid-column: 1 / -1;
-    width: 100%;
-  }
-
-  @media (max-width: 500px) {
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-template-columns: 1fr 1fr;
-    row-gap: 0px;
-    column-gap: 15px;
-    & > *:nth-child(1) {
-      grid-column: 1 / -1;
-      width: 100%;
-    }
-    & > *:nth-child(4) {
-      grid-column: 1 / -1;
-      width: 100%;
-    }
-  }
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  width: 100%;
 `;
 
 const Error = styled.small`
@@ -55,6 +30,11 @@ const Error = styled.small`
 
 const ButtonItem = styled.div`
   width: 90%;
+`;
+
+const InputLine = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const SignUpForm = () => {
@@ -73,7 +53,8 @@ const SignUpForm = () => {
   const onSubmit = (data: signUpUserInterface) => {
     const isConfirmed = window.confirm('회원가입을 완료하시겠습니까?');
     if (isConfirmed) {
-      mutate(data);
+      // mutate(data);
+      console.log(data);
     }
     console.log(data);
   };
@@ -81,96 +62,111 @@ const SignUpForm = () => {
   return (
     <AuthBox variant='primary'>
       <SignFormWrapper onSubmit={handleSubmit(onSubmit)}>
-        <SignupInput
-          id='name'
-          title='이름'
-          placeholder='ex) 홍길동'
-          type='text'
-          register={register('name')}
-        />
-        <ErrorMessage
-          errors={errors}
-          name='name'
-          render={({ message }) => <Error role='alert'>{message}</Error>}
-        />
-        <SignupInput
-          id='age'
-          title='나이'
-          placeholder='ex) 20'
-          type='text'
-          register={register('age', { valueAsNumber: true })}
-        />
-        <ErrorMessage
-          errors={errors}
-          name='age'
-          render={({ message }) => <Error role='alert'>{message}</Error>}
-        />
-        <SignupInput
-          id='studentNumber'
-          title='학번'
-          placeholder='ex) 2024111222'
-          type='text'
-          register={register('studentNumber')}
-        />
-        <ErrorMessage
-          errors={errors}
-          name='studentNumber'
-          render={({ message }) => <Error role='alert'>{message}</Error>}
-        />
-        <SignupInput
-          id='major'
-          title='전공'
-          placeholder='전공을 입력해주세요.'
-          type='string'
-          register={register('major')}
-        />
-        <ErrorMessage
-          errors={errors}
-          name='major'
-          render={({ message }) => <Error role='alert'>{message}</Error>}
-        />
+        <InputLine>
+          <SignupInput
+            id='name'
+            title='이름'
+            placeholder='ex) 홍길동'
+            type='text'
+            register={register('name')}
+          />
+          <ErrorMessage
+            errors={errors}
+            name='name'
+            render={({ message }) => <Error role='alert'>{message}</Error>}
+          />
+          <SignupInput
+            id='age'
+            title='나이'
+            placeholder='ex) 20'
+            type='text'
+            register={register('age', { valueAsNumber: true })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name='age'
+            render={({ message }) => <Error role='alert'>{message}</Error>}
+          />
+          <SignupInput
+            id='studentNumber'
+            title='학번'
+            placeholder='ex) 2024111222'
+            type='text'
+            register={register('studentNumber')}
+          />
+          <ErrorMessage
+            errors={errors}
+            name='studentNumber'
+            render={({ message }) => <Error role='alert'>{message}</Error>}
+          />
+        </InputLine>
+        <InputLine>
+          <SignupInput
+            id='major'
+            title='전공'
+            placeholder='전공의 정식명칭을 입력해주세요.'
+            type='string'
+            register={register('major')}
+          />
+          <ErrorMessage
+            errors={errors}
+            name='major'
+            render={({ message }) => <Error role='alert'>{message}</Error>}
+          />
+          <SignupInput
+            id='phoneNumber'
+            title='전화번호'
+            placeholder='010-0000-0000 형식으로 입력해주세요.'
+            type='string'
+            register={register('phoneNumber')}
+          />
+          <ErrorMessage
+            errors={errors}
+            name='phoneNumber'
+            render={({ message }) => <Error role='alert'>{message}</Error>}
+          />
+        </InputLine>
+        <ButtonItem>
+          {isPending && (
+            <CommonBtn
+              backgroundColor='blue'
+              color='blue'
+              hoverColor='blue'
+              type='submit'
+              size='lg'
+              width='100%'
+              height='45px'
+              padding='5px'
+              mdWidth='100%'
+              mdHeight='45px'
+              mWidth='100%'
+              mHeight='45px'
+              mPadding='5px'
+            >
+              Submitting...
+            </CommonBtn>
+          )}
+          {!isPending && (
+            <CommonBtn
+              backgroundColor='blue'
+              color='blue'
+              hoverColor='blue'
+              type='submit'
+              size='lg'
+              width='100%'
+              height='45px'
+              padding='5px'
+              mdWidth='100%'
+              mdHeight='45px'
+              mWidth='100%'
+              mHeight='45px'
+              mPadding='5px'
+            >
+              회원가입
+            </CommonBtn>
+          )}
+        </ButtonItem>
       </SignFormWrapper>
-
-      <ButtonItem>
-        {isPending && (
-          <CommonBtn
-            backgroundColor='blue'
-            color='blue'
-            hoverColor='blue'
-            type='submit'
-            size='lg'
-            width='100%'
-            height='45px'
-            padding='5px'
-            mdWidth='100%'
-            mdHeight='45px'
-            mWidth='100%'
-            mHeight='45px'
-            mPadding='5px'
-          >
-            Submitting...
-          </CommonBtn>
-        )}
-        {!isPending && (
-          <CommonBtn
-            backgroundColor='blue'
-            color='blue'
-            hoverColor='blue'
-            type='submit'
-            size='lg'
-            width='100%'
-            height='45px'
-            padding='5px'
-            mdWidth='100%'
-            mdHeight='45px'
-            mWidth='100%'
-            mHeight='45px'
-            mPadding='5px'
-          >
-            회원가입
-          </CommonBtn>
-        )}
-      </ButtonItem>
     </AuthBox>
   );
 };
