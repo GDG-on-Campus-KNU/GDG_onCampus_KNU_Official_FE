fa03cc9fcae876758e5fea2fd37e992c6c81e474
KimKyuHoi <k546kh@gmail.com>
2024-07-15 16:49:17 +0900

    Feat: Schema 추가

--- src/utils/SignUpSchema.util.ts
+++ src/utils/SignUpSchema.util.ts
@@ -6,7 +6,7 @@ export const SignUpSchema = z.object({
   name: z.string().min(1, { message: '🚨 이름은 필수로 입력해주셔야 합니다.' }),
   age: z
     .number({
-      invalid_type_error: '🚨 나이는 필수로 입력해주셔야 합니다.',
+      invalid_type_error: '🚨 나이는 숫자형식으로 입력해주셔야 합니다.',
     })
     .min(1, { message: '🚨 나이는 1살부터 입력이 가능합니다.' }),
   studentNumber: z
@@ -16,4 +16,7 @@ export const SignUpSchema = z.object({
   major: z
     .string()
     .min(1, { message: '🚨 전공을 필수로 입력해주셔야 합니다.' }),
+  phoneNumber: z.string().regex(/^\d{3}-\d{4}-\d{4}$/, {
+    message: '🚨 올바른 전화번호 형식을 입력해주세요. 예: 000-0000-0000',
+  }),
 });
