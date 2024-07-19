126ff94431e61fa3f90d653d804b4e81eabdb09f
KimKyuHoi <k546kh@gmail.com>
2024-07-19 13:09:50 +0900

    Feat: MyData받아오기 구현

--- src/apis/hooks/mypage/useGetMyData.ts
+++ src/apis/hooks/mypage/useGetMyData.ts
@@ -1,19 +1,20 @@
-import { instanceJWT } from '@gdsc/apis/instance/Api_JWT';
+import { fetchInstance } from '@gdsc/apis/instance/Api_JWT';
 
+import { userDataInterface } from '@gdsc/types/UserInterface';
 import { useQuery } from '@tanstack/react-query';
 
 const getMyDataPath = () => '/api/user';
 
 const myDataQueryKey = [getMyDataPath()];
 
-export const getMyData = async () => {
-  const response = await instanceJWT.get(getMyDataPath());
+export const getMyData = async (): Promise<userDataInterface> => {
+  const response = await fetchInstance.get<userDataInterface>(getMyDataPath());
 
   return response.data;
 };
 
 export const useGetMyData = () =>
-  useQuery({
+  useQuery<userDataInterface>({
     queryKey: myDataQueryKey,
     queryFn: getMyData,
   });
