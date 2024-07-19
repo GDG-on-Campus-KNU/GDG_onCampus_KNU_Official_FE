cb6fb804d07bf416fef976b246b0f34cb1659bf7
KimKyuHoi <k546kh@gmail.com>
2024-07-19 23:33:06 +0900

    Feat: Guest, Core, Lead일 경우 추가정보 입력페이지에 접근할 경우 리다이렉트

--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import SignUpForm from '@gdsc/pages/signup/components/SignUpForm';
+
+import { useGetMyData } from '@gdsc/apis/hooks/mypage/useGetMyData';
 
 import { AuthWrapper } from '@gdsc/styles/AuthModalStyle';
 
 const SignupPage = () => {
+  const { data: MyData } = useGetMyData();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (MyData && MyData?.role !== 'ROLE_TEMP') {
+      alert('추가 정보를 입력한 상태입니다. 메인페이지로 이동합니다.');
+      navigate('/');
+    }
+  }, [MyData, navigate]);
+
   return (
     <AuthWrapper>
       <SignUpForm />
