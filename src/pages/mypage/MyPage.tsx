145a8950d84f042428957371b7c60773206375f1
chae-won-shin <scwcart@naver.com>
2024-07-21 02:01:59 +0900

    Feat: 마이페이지 사용자 정보 조회 및 수정 기능 구현

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
+
+import axios from 'axios';
 
 import CompleteBtn from '@gdsc/components/common/button/CompleteBtn';
 import Input from '@gdsc/components/common/form/Input';
@@ -9,7 +12,9 @@ import PageTitle from '@gdsc/components/common/title/PageTitle';
 import { displayCenter } from '@gdsc/styles/LayoutStyle';
 
 import TeamToken from './components/TeamToken';
+import { useUserInfo } from './hooks/useUserInfo';
 import styled from '@emotion/styled';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const MyPageWrapper = styled.div<{ color: string }>`
   ${displayCenter}
@@ -137,23 +142,101 @@ const ButtonContainer = styled.div`
   }
 `;
 
+interface IUserInfo {
+  name: string;
+  age: number;
+  major: string;
+  studentNumber: string;
+  email: string;
+  teamInfos: string[];
+}
+
 const MyPage = () => {
+  const [userInfo, setUserInfo] = useState<IUserInfo>({
+    name: '',
+    age: 0,
+    major: '',
+    studentNumber: '',
+    email: '',
+    teamInfos: [],
+  });
+
   const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+  const accessToken = localStorage.getItem('accessToken');
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, isError } = useUserInfo(accessToken);
+
+  useEffect(() => {
+    if (data) {
+      setUserInfo(data);
+    }
+  }, [data]);
+
+  const updateUserMutation = useMutation({
+    mutationFn: (updatedInfo: IUserInfo) =>
+      axios.put('http://43.202.198.49:8080/api/user', updatedInfo, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['userInfo'] });
+      alert('정보가 성공적으로 저장되었습니다.');
+    },
+    onError: (error: string) => {
+      console.error('정보 저장 중 오류가 발생했습니다:', error);
+      alert('정보 저장에 실패했습니다.');
+    },
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setUserInfo((prev) => ({
+      ...prev,
+      [id]: id === 'age' ? parseInt(value) : value,
+    }));
+  };
+
+  const handleSave = () => {
+    updateUserMutation.mutate(userInfo);
+  };
+
+  if (isLoading) {
+    console.log('loading...');
+  }
+  if (isError) {
+    console.log('사용자 정보를 가져오는 중 에러가 발생했습니다.');
+  }
+
+  // const teamInfo = [
+  //   'FE study 2팀',
+  //   'Solution Challenge 3팀',
+  //   '2차 프로젝트 1팀',
+  //   'FE study 2팀',
+  // ];
+
   return (
     <>
       <PageTitle MainTitle='마이페이지' SubTitle='My Page' />
       <MyPageWrapper color='var(--color-abony)'>
         <MainInfoContainer>
           <Profile />
-          <Input id='name' label='이름' placeholder='ex) 홍길동' type='text' />
+          <Input
+            id='name'
+            label='이름'
+            placeholder='ex) 홍길동'
+            type='text'
+            value={userInfo.name}
+            onChange={handleInputChange}
+          />
           {isMobile ? <></> : <EmptyDiv></EmptyDiv>}
           <EmptyDiv>
             <InputLabel>소속 팀</InputLabel>
             <TokenContainer>
-              <TeamToken teamname='FE study 2팀' />
-              <TeamToken teamname='Solution Challenge 3팀' />
-              <TeamToken teamname='2차 프로젝트 1팀' />
-              <TeamToken teamname='FE study 2팀' />
+              {userInfo.teamInfos.map((e, i) => {
+                return <TeamToken key={`${e}-${i}`} teamname={e} />;
+              })}
             </TokenContainer>
           </EmptyDiv>
         </MainInfoContainer>
@@ -168,46 +251,72 @@ const MyPage = () => {
         />
         {isMobile ? (
           <MobileSubInfoContainer>
-            <Input id='age' label='나이' placeholder='ex) 20' type='number' />
+            <Input
+              id='age'
+              label='나이'
+              placeholder='ex) 20'
+              type='number'
+              value={`${userInfo.age}`}
+              onChange={handleInputChange}
+            />
             <Input
               id='major'
               label='전공'
               placeholder='ex) 컴퓨터학부 글로벌소프트웨어융합전공'
               type='text'
+              value={userInfo.major}
+              onChange={handleInputChange}
             />
             <Input
               id='studentNumber'
               label='학번'
               placeholder='ex)2024111222'
               type='text'
+              value={userInfo.studentNumber}
+              onChange={handleInputChange}
             />
             <Input
               id='email'
               label='이메일'
               placeholder='ex) honggildong@gmail.com'
               type='text'
+              value={userInfo.email}
+              onChange={handleInputChange}
             />
           </MobileSubInfoContainer>
         ) : (
           <SubInfoContainer>
-            <Input id='age' label='나이' placeholder='ex) 20' type='number' />
+            <Input
+              id='age'
+              label='나이'
+              placeholder='ex) 20'
+              type='number'
+              value={`${userInfo.age}`}
+              onChange={handleInputChange}
+            />
             <Input
               id='major'
               label='전공'
               placeholder='ex) 컴퓨터학부 글로벌소프트웨어융합전공'
               type='text'
+              value={userInfo.major}
+              onChange={handleInputChange}
             />
             <Input
               id='studentNumber'
               label='학번'
               placeholder='ex)2024111222'
               type='text'
+              value={userInfo.studentNumber}
+              onChange={handleInputChange}
             />
             <Input
               id='email'
               label='이메일'
               placeholder='ex) honggildong@gmail.com'
               type='text'
+              value={userInfo.email}
+              onChange={handleInputChange}
             />
           </SubInfoContainer>
         )}
@@ -219,6 +328,7 @@ const MyPage = () => {
           backgroundColor='blue'
           hoverColor='white'
           type='submit'
+          onClick={handleSave}
         >
           저장하기
         </CompleteBtn>
