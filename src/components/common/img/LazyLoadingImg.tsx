cc5277365dc938f107ded1e48ab1f15536a11f58
KimKyuHoi <k546kh@gmail.com>
2024-08-06 21:08:05 +0900

    Feat: lazyloading 이미지 공용컴포넌트 구현

---
+++ src/components/common/img/LazyLoadingImg.tsx
@@ -0,0 +1,19 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+export const LazyLoadImg = ({
+  image,
+}: {
+  image: { alt: string; src: string; width?: number; height?: number };
+}) => (
+  <LazyLoadImage
+    alt={image.alt}
+    effect='blur'
+    wrapperProps={{
+      style: { transitionDelay: '0.7s' },
+    }}
+    src={image.src}
+    width={image.width}
+    height={image.height}
+  />
+);
