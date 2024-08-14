b706f7cdcf67a1e3639044f3a09f0ad905d2c57d
KimKyuHoi <k546kh@gmail.com>
2024-08-15 01:23:10 +0900

    Feat: 모달창 구현

---
+++ src/styles/GlobalModal.style.ts
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+
+import styled from '@emotion/styled';
+
+export const ModalOverlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+export const ModalBackdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-abony);
+  opacity: 0.6;
+  z-index: 999;
+`;
