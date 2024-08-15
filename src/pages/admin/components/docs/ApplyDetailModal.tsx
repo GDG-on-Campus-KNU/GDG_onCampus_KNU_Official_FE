1881606efd113c5978dc390d104b7190d4ca7547
chae-won-shin <scwcart@naver.com>
2024-08-15 22:52:21 +0900

    Refactor: 스타일 코드 분리

--- src/pages/admin/components/docs/ApplyDetailModal.tsx
+++ src/pages/admin/components/docs/ApplyDetailModal.tsx
@@ -1,142 +1,23 @@
 import CommonBtn from '@gdsc/components/common/button/CommonBtn';
 import Text from '@gdsc/components/common/typography/Text';
 
-import CloseIcon from '@gdsc/assets/CloseIcon.svg';
-
-import { displayCenter } from '@gdsc/styles/LayoutStyle';
-
+import {
+  ModalBackdrop,
+  ModalWrapper,
+  TitleWrapper,
+  CloseBtn,
+  ContentWrapper,
+  IntroContainer,
+  ProfileContainer,
+  DividingLine,
+  SelfIntroduce,
+  ButtonContainer,
+} from './ApplyDetailModal.style';
 import ApplyInfo from './ApplyInfo';
 import BasicInfo from './BasicInfo';
 import Memo from './Memo';
 import Stars from './Stars';
 import TechStack from './TechStack';
-import styled from '@emotion/styled';
-
-const ModalBackdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-`;
-
-const ModalWrapper = styled.div`
-  width: 90%;
-  height: 85vh;
-  min-width: 900px;
-  min-height: 500px;
-
-  ${displayCenter}
-  flex-direction: column;
-  align-items: center;
-
-  background-color: var(--color-alto);
-
-  padding: 15px 20px;
-  box-sizing: border-box;
-  border-radius: 12px;
-  box-shadow: 5px 5px 10px 0 rgba(25, 5, 60, 0.25);
-
-  position: relative;
-  top: 53%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1000;
-`;
-
-const TitleWrapper = styled.h3`
-  width: 100%;
-
-  padding: 0;
-  margin: 0;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const CloseBtn = styled.button`
-  width: 18px;
-  height: 18px;
-  background-image: url(${CloseIcon});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-color: transparent;
-
-  border: none;
-  outline: none;
-  cursor: pointer;
-
-  &:hover {
-    transform: scale(1.1);
-  }
-`;
-
-const ContentWrapper = styled.div`
-  width: 100%;
-  height: 90%;
-  ${displayCenter}
-  gap: 15px;
-`;
-
-const IntroContainer = styled.div`
-  width: 60%;
-  height: 90%;
-
-  overflow-y: auto;
-
-  background-color: var(--color-white);
-
-  padding: 15px 25px;
-  margin-top: 10px;
-  border-radius: 12px;
-  box-shadow: 5px 5px 10px 0 rgba(25, 5, 60, 0.25);
-`;
-
-const ProfileContainer = styled.div`
-  width: 40%;
-  height: 90%;
-
-  overflow-y: auto;
-
-  background-color: var(--color-white);
-
-  padding: 15px 25px;
-  margin-top: 10px;
-  border-radius: 12px;
-  box-shadow: 5px 5px 10px 0 rgba(25, 5, 60, 0.25);
-`;
-
-const DividingLine = styled.div`
-  width: 100%;
-  height: 1px;
-
-  background-color: var(--color-dove);
-
-  margin: 15px 0px;
-`;
-
-const SelfIntroduce = styled.div`
-  width: 100%;
-  height: 200px;
-
-  overflow-y: scroll;
-
-  color: black;
-  font-size: var(--font-size-xs);
-  font-weight: 200;
-
-  margin-top: 5px;
-`;
-
-const ButtonContainer = styled.div`
-  width: 100%;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 const ApplyDetailModal = () => {
   return (
