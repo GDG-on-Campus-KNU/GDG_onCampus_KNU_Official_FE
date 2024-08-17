7ad8bbb423b3a5325f967d289a06ecd9d04a41d1
chae-won-shin <scwcart@naver.com>
2024-08-17 22:37:56 +0900

    Style: 소개페이지 직렬카드 모바일버전 padding값 및 폰트사이즈 조정

--- src/pages/introduce/components/FieldTable.tsx
+++ src/pages/introduce/components/FieldTable.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 import gsap from 'gsap';
 
@@ -21,6 +22,13 @@ const FieldTableBox = styled.div`
   @media (max-width: 767px) {
     grid-template-columns: 1fr;
     row-gap: 33px;
+  }
+
+  @media (max-width: 500px) {
+    grid-template-columns: 1fr;
+    row-gap: 33px;
+    padding: 0 40px;
+    justify-items: center;
   }
 
   &.animateFieldTable {
@@ -71,6 +79,10 @@ const HighlightText = styled(Text)<{ highlightColor?: string }>`
 `;
 
 const FieldTable = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+  const titleSize = isMobile ? 'lg' : 'xl';
+  const expSize = isMobile ? 'sm' : 'md';
+
   useEffect(() => {
     gsap.to('.animateFieldTable', {
       scrollTrigger: {
@@ -93,13 +105,13 @@ const FieldTable = () => {
           <HighlightText
             color='black'
             weight='600'
-            size='xl'
+            size={titleSize}
             highlightColor='#F7B4AE'
           >
             FRONTEND DEVELOPER
           </HighlightText>
           <ExplainLayout>
-            <Text color='black' size='md'>
+            <Text color='black' size={expSize}>
               사용자와 서비스가 직접 상호작용하는 부분을 담당합니다. 웹이나 앱을
               시각화하고, 사용자들이 쉽고 빠르게 사용할 수 있도록 서비스를
               구축하고 개선합니다.
@@ -112,13 +124,13 @@ const FieldTable = () => {
           <HighlightText
             color='black'
             weight='600'
-            size='xl'
+            size={titleSize}
             highlightColor='#9FD8BC'
           >
             BACKEND DEVELOPER
           </HighlightText>
           <ExplainLayout>
-            <Text color='black' size='md'>
+            <Text color='black' size={expSize}>
               웹이나 앱의 보이지 않는 서버를 담당합니다. 사용자가 웹이나 앱과
               상호작용할 때 필요한 서버, 데이터베이스, 애플리케이션 로직을
               개발하고 유지보수합니다.
@@ -131,13 +143,13 @@ const FieldTable = () => {
           <HighlightText
             color='black'
             weight='600'
-            size='xl'
+            size={titleSize}
             highlightColor='#B3CEFB'
           >
             AI ENGINEER
           </HighlightText>
           <ExplainLayout>
-            <Text color='black' size='md'>
+            <Text color='black' size={expSize}>
               인공지능 시스템을 설계하는 엔지니어입니다. 인공지능 기술을
               활용하여 업무를 자동화하거나 인사이트를 도출하는 등 각종 문제를
               해결합니다.
@@ -150,13 +162,13 @@ const FieldTable = () => {
           <HighlightText
             color='black'
             weight='600'
-            size='xl'
+            size={titleSize}
             highlightColor='#FDE49B'
           >
             ANDROID DEVELOPER
           </HighlightText>
           <ExplainLayout>
-            <Text color='black' size='md'>
+            <Text color='black' size={expSize}>
               모바일 애플리케이션 설계, 개발 및 유지보수를 담당합니다.
               안드로이드 플랫폼에서의 소프트웨어 개발에 특화되어 사용자 경험을
               개선하고 기능을 구현합니다.
@@ -169,13 +181,13 @@ const FieldTable = () => {
           <HighlightText
             color='black'
             weight='600'
-            size='xl'
+            size={titleSize}
             highlightColor='#D1B3FD'
           >
             DESIGNER
           </HighlightText>
           <ExplainLayout>
-            <Text color='black' size='md'>
+            <Text color='black' size={expSize}>
               사용자 조사, 정보 구조 설계, 와이어 프레임 및 프로토타입 제작 등의
               작업을 담당합니다. 디자이너는 사용자의 니즈와 목표를 이해하고,
               해당 제품 또는 서비스가 사용자에게 제공하는 가치를 극대화하기 위해
