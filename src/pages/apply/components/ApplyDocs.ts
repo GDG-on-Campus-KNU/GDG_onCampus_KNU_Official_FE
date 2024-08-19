2c77a4ba0558d541c6867b90c392ecd024597454
Dobbymin <gmin0701@knu.ac.kr>
2024-08-19 13:20:55 +0900

    Hotfix!: 지원기간 수정

--- src/pages/apply/components/ApplyDocs.ts
+++ src/pages/apply/components/ApplyDocs.ts
@@ -4,7 +4,7 @@ export const FrontendData = {
   explain: `사용자와 서비스가 직접 상호작용하는 부분을 담당합니다. 웹이나
               앱을 시각화하고, 사용자들이 쉽고 빠르게 사용할 수 있도록
               서비스를 구축하고 개선합니다.`,
-  duedate: '2024.08.24',
+  duedate: '2024.09.05',
   link: 'form',
   recruit: [
     '개발에 관심있고 열정 있으신 분',
@@ -34,7 +34,7 @@ export const BackendData = {
   english: 'Backend Developer',
   explain:
     '웹이나 앱의 보이지 않는 서버를 담당합니다. 사용자가 웹이나 앱과 상호작용할 때 필요한 서버, 데이터베이스, 애플리케이션 로직을 개발하고 유지보수합니다.',
-  duedate: '2024.08.24',
+  duedate: '2024.09.05',
   link: 'form',
   recruit: [
     '개발에 관심있고 열정 있으신 분',
@@ -62,7 +62,7 @@ export const AIData = {
   english: 'AI Developer',
   explain:
     '인공지능 시스템을 설계하는 엔지니어입니다. 인공지능 기술을 활용하여 업무를 자동화하거나 인사이트를 도출하는 등 각종 문제를 해결합니다.',
-  duedate: '2024.08.24',
+  duedate: '2024.09.05',
   link: 'form',
   recruit: [
     '개발에 관심있고 열정 있으신 분',
@@ -89,7 +89,7 @@ export const AndroidData = {
   english: 'Android Developer',
   explain:
     '모바일 애플리케이션 설계, 개발 및 유지보수를 담당합니다. 안드로이드 플랫폼에서의 소프트웨어 개발에 특화되어 사용자 경험을 개선하고 기능을 구현합니다.',
-  duedate: '2024.08.24',
+  duedate: '2024.09.05',
   link: 'form',
   recruit: [
     '개발에 관심있고 열정 있으신 분',
@@ -118,7 +118,7 @@ export const DesignerData = {
   english: 'Designer',
   explain:
     '사용자 조사, 정보 구조 설계, 와이어 프레임 및 프로토타입 제작 등의 작업을 담당합니다. 디자이너는 사용자의 니즈와 목표를 이해하고, 해당 제품 또는 서비스가 사용자에게 제공하는 가치를 극대화하기 위해 최적의 경험을 설계합니다.',
-  duedate: '2024.08.24',
+  duedate: '2024.09.05',
   link: 'form',
   recruit: [
     '디자인에 관심있고 열정 있으신 분',
