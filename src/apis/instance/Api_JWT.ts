4f284818f077a3232ac74e866083908c4675bf2c
KimKyuHoi <k546kh@gmail.com>
2024-09-27 12:30:52 +0900

    Remove: console.log 제거

--- src/apis/instance/Api_JWT.ts
+++ src/apis/instance/Api_JWT.ts
@@ -38,7 +38,6 @@ export const createInstanceJWT = (
   const isTokenExpired = (token: string | null): boolean => {
     if (!token) return true;
     const decodedToken: { exp: number } = jwtDecode(token);
-    console.log;
     const currentTime = Math.floor(Date.now() / 1000);
     return decodedToken.exp < currentTime;
   };
