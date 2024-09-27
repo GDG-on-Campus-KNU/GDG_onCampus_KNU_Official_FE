762fd4ece0432755a6f83d7a4a2bea2725603438
KimKyuHoi <k546kh@gmail.com>
2024-09-27 11:46:17 +0900

    Fix: api 경로 수정

--- src/apis/hooks/signup/SignupAPI.ts
+++ src/apis/hooks/signup/SignupAPI.ts
@@ -6,7 +6,7 @@ export const SignupAPI = (
   userData: signUpUserInterface
 ): Promise<signUpUserInterface> => {
   return fetchInstance
-    .post('/api/user/additional-info', userData)
+    .post('/api/user', userData)
     .then(function (response) {
       // console.log(response);
       return response.data;
