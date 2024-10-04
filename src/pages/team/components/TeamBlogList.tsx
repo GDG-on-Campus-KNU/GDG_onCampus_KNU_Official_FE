b9c6c2f908fad11f346450892bceb25c11a72b56
KimKyuHoi <k546kh@gmail.com>
2024-10-04 16:48:18 +0900

    feat: TeamBlogList 구현하기

--- src/pages/team/components/TeamBlogList.tsx
+++ src/pages/team/components/TeamBlogList.tsx
@@ -1,21 +1,19 @@
-import MaksGroup from '@gdg/assets/thumbnail/Mask group.png';
 import { Grid } from '@gdg/components/common/layouts/grid';
 import ThumbNailCard from '@gdg/components/feature/blog/ThumbNailCard';
+
+import MaskGroup from '@gdg/assets/thumbnail/Mask group.png';
 
 const TeamBlogList = () => {
   return (
     <Grid
-      columns={{
-        sm: 1,
-        md: 2,
-        lg: 2,
-      }}
-      gap={50}
+      columns={{ lg: 2, md: 1, sm: 1 }}
+      padding={{ lg: 62, md: 62, sm: 70 }}
+      gap={{ lg: 50, md: 50, sm: 30 }}
     >
       {Array.from({ length: 8 }).map((_, index) => (
         <ThumbNailCard
           key={index}
-          src={MaksGroup}
+          src={MaskGroup}
           title='좌충우돌 GDG on Campus KNU 4기 준비하기 - 리드편'
           subtitle='이번 3기 리드님의 추천으로 GDSC Korea에 지원을 하게 되었는데 이번 기회에 Lead를 합격하게 되었다!
           총 한국에서는 총 37개의 대학이 참여를 하게 되었고, 활동은 24년 8월부터 25년 7월까지 활동을 하게 되었다!'
