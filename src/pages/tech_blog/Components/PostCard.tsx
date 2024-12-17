d8ea139d52cd2fefa89d1dbb7a62a06f98754ada
chae-won-shin <scwcart@naver.com>
2024-12-18 02:41:37 +0900

    style: 테크블로그 페이지 퍼블리싱

---
+++ src/pages/tech_blog/Components/PostCard.tsx
@@ -0,0 +1,43 @@
+import Text from '@gdg/components/common/typography/Text';
+import gdgknu from '@gdg/assets/gdgknu.png';
+import likes from '@gdg/assets/icon/likes.svg';
+import comment from '@gdg/assets/icon/comment.svg';
+
+import {
+  CardWrapper,
+  Image,
+  MetaDataLayout,
+  TrackCard,
+  Icon,
+} from './PostCard.style';
+
+const PostCard = () => {
+  return (
+    <CardWrapper>
+      <Image src={gdgknu} alt='thumbnail' />
+      <MetaDataLayout $gap={8} $dir='column'>
+        <Text size='xl' weight='bold'>
+          테크 블로그 글 제목
+        </Text>
+        <MetaDataLayout $gap={6} $dir='column'>
+          <Text size='md'>본문 미리보기 또는 부제목</Text>
+          <MetaDataLayout $gap={12} $dir='row'>
+            <TrackCard>프론트엔드</TrackCard>
+            <Text>2024.08.24</Text>
+            <Text>|</Text>
+            <MetaDataLayout $gap={5} $dir='row'>
+              <Icon src={likes} alt='likes' />
+              <Text>123</Text>
+            </MetaDataLayout>
+            <MetaDataLayout $gap={5} $dir='row'>
+              <Icon src={comment} alt='likes' />
+              <Text>456</Text>
+            </MetaDataLayout>
+          </MetaDataLayout>
+        </MetaDataLayout>
+      </MetaDataLayout>
+    </CardWrapper>
+  );
+};
+
+export default PostCard;
--- src/pages/tech_blog/TechBlogPage.tsx
+++ src/pages/tech_blog/TechBlogPage.tsx
@@ -1,12 +1,50 @@
 // import { TeamBlogMetaData } from '@gdg/router/components/MetaData';
+import styled from '@emotion/styled';
 
-import CommingSoonPage from '../CommingSoonPage';
+import Text from '@gdg/components/common/typography/Text';
+import Grid from '@gdg/components/common/layouts/grid';
+import PageTitle from '@gdg/components/common/title/PageTitle';
+
+import PostCard from './Components/PostCard';
+
+const PostListLayout = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  margin: 20px 45px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
 
 const TechBlogPage = () => {
+  const popular = ['', '', '', '', ''];
+  const latest = ['', '', '', '', '', ''];
   return (
     <>
       {/* <TeamBlogMetaData /> */}
-      <CommingSoonPage />
+      <PageTitle MainTitle='테크 블로그' SubTitle='Tech Blog' />
+      <PostListLayout>
+        <Text size='xl' weight='bold'>
+          인기글 TOP 5
+        </Text>
+        <Grid columns={2} gap={36} padding={0}>
+          {popular.map((e, i) => {
+            return <PostCard key={i} />;
+          })}
+        </Grid>
+      </PostListLayout>
+
+      <PostListLayout>
+        <Text size='xl' weight='bold'>
+          최신글
+        </Text>
+        <Grid columns={2} gap={36} padding={0}>
+          {latest.map((e, i) => {
+            return <PostCard key={i} />;
+          })}
+        </Grid>
+      </PostListLayout>
     </>
   );
 };
