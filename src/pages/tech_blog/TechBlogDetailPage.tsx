e48b86bc43488d3178a0c9188e6ecf520c0f0399
chae-won-shin <scwcart@naver.com>
2025-02-25 18:49:18 +0900

    feat: 상세페이지 스크롤 맨 위로 뜨도록 구현

--- src/pages/tech_blog/TechBlogDetailPage.tsx
+++ src/pages/tech_blog/TechBlogDetailPage.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import { Spinner } from '@gdg/components/common/Spinner';
 import { useGetPostDetail } from '@gdg/apis/hooks/techblog/useGetPostDetail';
@@ -12,6 +13,10 @@ const TechBlogDetailPage = () => {
   const postId = userId ? parseInt(userId) : null;
 
   const postData = useGetPostDetail(postId);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
