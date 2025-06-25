27fb94ad353e4a1377500f3bcab1a608896cb05f
KimKyuHoi <k546kh@gmail.com>
2025-06-25 19:59:30 +0900

    feat: 컨텐츠 url 추가

--- src/pages/main/components/MainIntroduceText.ts
+++ src/pages/main/components/MainIntroduceText.ts
@@ -1,32 +1,39 @@
 export const cardData = [
   {
-    mainText: 'GDG on Campus KNU',
-    subText: 'GDG on Campus KNU Team Blog',
-    contentText: 'GDG on Campus KNU 팀의 활동과 정보를 담은 블로그입니다.',
+    mainText: 'Devfest:CareerTalk',
+    subText: 'GDGoC KNU',
+    contentText:
+      'GDGoC KNU에서 활동하신 현직자들과 함께한 토크콘서트 행사입니다.',
+    link: 'https://devfest-careertalks.netlify.app/',
   },
   {
-    mainText: '프론트엔드 팀 블로그',
-    subText: 'Frontend Team Blog',
-    contentText: '프론트엔드 팀의 활동과 정보를 담은 블로그입니다.',
-  },
-  {
-    mainText: 'AI 팀 블로그',
-    subText: 'AI Team Blog',
-    contentText: 'AI 팀의 활동과 정보를 담은 블로그입니다.',
+    mainText: 'Vision Challenge Hackathon',
+    subText: 'Vision Chllenge',
+    contentText: 'Google Cloud, 경북대 SW 교육원과 협력한 해커톤입니다.',
+    link: 'https://vision.gdsc-knu.com/',
   },
   {
     mainText: '안드로이드 팀 블로그',
     subText: 'Android Team Blog',
     contentText: '안드로이드 팀의 활동과 정보를 담은 블로그입니다.',
+    link: '/techblog',
   },
   {
     mainText: '디자이너 팀 블로그',
     subText: 'Designer Team Blog',
     contentText: '디자이너 팀의 활동과 정보를 담은 블로그입니다.',
+    link: '/techblog',
   },
   {
     mainText: '백엔드 팀 블로그',
     subText: 'Backend Team Blog',
     contentText: '백엔드 팀의 활동과 정보를 담은 블로그입니다.',
+    link: '/techblog',
+  },
+  {
+    mainText: 'GDG on Campus KNU',
+    subText: 'GDG on Campus KNU Team Blog',
+    contentText: 'GDG on Campus KNU 팀의 활동과 정보를 담은 블로그입니다.',
+    link: '/techblog',
   },
 ];
