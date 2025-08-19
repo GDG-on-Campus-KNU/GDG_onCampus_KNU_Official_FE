c7c6fd8e0cce322fd330538a234a9f44ac0cc897
KimDaeGeon <52105661+toothlessdev@users.noreply.github.com>
2025-08-19 16:59:34 +0900

    feat: add fifth class year option

--- src/apis/hooks/yearId/useGetClassYearList.ts
+++ src/apis/hooks/yearId/useGetClassYearList.ts
@@ -15,7 +15,22 @@ const teamListQueryKey = [getClassYearListPath()];
 
 const getClassYearList = async () => {
   const response = await fetchInstance.get(getClassYearListPath());
-  return response.data;
+  const data: classYearList[] = response.data;
+
+  // 신규 기수(5기)를 추가해 드롭다운에서 선택할 수 있도록 합니다.
+  const hasFifth = data.some(
+    (year) => year.id === 5 || year.name === '5기'
+  );
+  if (!hasFifth) {
+    data.push({
+      id: 5,
+      name: '5기',
+      applyStartDateTime: '',
+      applyEndDateTime: '',
+    });
+  }
+
+  return data;
 };
 
 export const useGetClassYearList = (): UseQueryResult<
